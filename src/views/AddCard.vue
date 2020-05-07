3999897d3a6972da76c4550184fcff27d353ce81
Santiago Bandiera <sagaban@hotmail.com>
2020-05-07 19:02:29 -0300

    Add basic form for new cards

--- src/views/AddCard.vue
+++ src/views/AddCard.vue
@@ -1,5 +1,123 @@
 <template>
-  <div class="add-card">
-    <h1>AddCard</h1>
+  <div class="q-pa-md add-card">
+    <h4>Add a new word</h4>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input
+        filled
+        v-model="word"
+        label="Word to learn"
+        lazy-rules
+        :rules="[val => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-input
+        filled
+        v-model="translation"
+        label="Word translation"
+        lazy-rules
+        :rules="[val => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-input
+        filled
+        v-model="notes"
+        type="textarea"
+        label="Notes"
+        lazy-rules
+      />
+
+      <q-select
+        use-input
+        input-debounce="0"
+        multiple
+        use-chips
+        @new-value="createTag"
+        @filter="tagFilter"
+        v-model="tags"
+        :options="filteredOptions"
+        label="Tags"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
   </div>
 </template>
+
+<script>
+export default {
+  name: "AddWord",
+  data() {
+    return {
+      word: null,
+      translation: null,
+      notes: null,
+      tags: null,
+      tagOptions: ["animal", "buildings", "fruits"],
+      filteredOptions: ["animal", "buildings", "fruits"]
+    };
+  },
+  methods: {
+    tagFilter(val, update) {
+      console.log(val);
+      if (val === "") {
+        update(() => {
+          this.filteredOptions = this.tagOptions;
+        });
+        return;
+      }
+
+      update(() => {
+        const needle = val.toLowerCase();
+        this.filteredOptions = this.tagOptions.filter(v =>
+          v.toLowerCase().includes(needle)
+        );
+      });
+    },
+    createTag(val, done) {
+      if (val.length > 0) {
+        const tags = (this.tags || []).slice();
+
+        val
+          .split(/[,;|]+/)
+          .map(v => v.trim())
+          .filter(v => v.length > 0)
+          .forEach(v => {
+            if (!this.tagOptions.includes(v)) {
+              this.tagOptions.push(v);
+            }
+            if (!tags.includes(v)) {
+              tags.push(v);
+            }
+          });
+
+        done(tags);
+        this.tags = tags;
+      }
+    },
+    onSubmit() {
+      console.log("submit");
+    },
+
+    onReset() {
+      this.word = null;
+      this.translation = null;
+      this.notes = null;
+      this.tags = [];
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.add-card {
+  max-width: 30rem;
+  margin: 0 auto;
+}
+</style>
